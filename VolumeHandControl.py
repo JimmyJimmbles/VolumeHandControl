7e7e6544276a7d48ce88a75e616a35d6bffd24c9
Jay Jones <jaypjonesofficial@gmail.com>
2021-06-05 13:35:28 -0500

    Cehcking what fingers are up to set volume

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -1,29 +1,37 @@
 import cv2
 import time
 import numpy as np
-import modules.HandTrackingModule as htm
 from modules.WebcamVideoStream import WebcamVideoStream
+from modules.HandTrackingModule import HandDetector
 import math
-import osascript
-
+from Foundation import NSAppleScript
 
 # Video Capture
 wvs = WebcamVideoStream().start()
-# cap = cv2.VideoCapture(0)
 
 # Get the fps
 prev_time = 0
 
 # HTM Object
-detector = htm.HandDetector(detection_confidence=0.7)
-vol = osascript.osascript("output volume of (get volume settings)")
+detector = HandDetector(detection_confidence=0.7)
+setVol = True
 
 while True:
-  # succ, img = cap.read()
+  # Get the frame
   frame = wvs.read()
+
+  # Find hands
   frame = detector.find_hands(frame)
   lm_list = detector.find_position(frame, draw=False)
 
+  # Get current volume
+  s = NSAppleScript.alloc().initWithSource_('output volume of (get volume settings)')
+  result, error_info = s.executeAndReturnError_(None)
+  vol = int(result.stringValue())
+
+  # Display volume bar
+  cv2.rectangle(frame, (75, (302 - (2 * vol))), (10, 400), (252, 165, 3), cv2.FILLED)
+  cv2.putText(frame, 'Vol: {} %'.format(vol), (10, 440), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (252, 165, 3), 2)
 
   if len(lm_list) != 0:
     # Get volume settings
@@ -31,39 +39,50 @@ while True:
     max_volume = 100
 
     # Coords for the index finder and thumb
-    x1, y1 = lm_list[4][1], lm_list[4][2]
-    x2, y2 = lm_list[8][1], lm_list[8][2]
+    t1, t2 = lm_list[4][1], lm_list[4][2]
+    i1, i2 = lm_list[8][1], lm_list[8][2]
 
     # Center of line
-    cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+    cx, cy = (t1 + i1) // 2, (t2 + i2) // 2
 
-    cv2.circle(frame, (x1, y1), 15, (252, 165, 3), cv2.FILLED)
-    cv2.circle(frame, (x2, y2), 15, (252, 165, 3), cv2.FILLED)
-    cv2.line(frame, (x1, y1), (x2, y2), (252, 165, 3), 3)
+    cv2.circle(frame, (t1, t2), 15, (252, 165, 3), cv2.FILLED)
+    cv2.circle(frame, (i1, i2), 15, (252, 165, 3), cv2.FILLED)
+    cv2.line(frame, (t1, t2), (i1, i2), (252, 165, 3), 3)
     cv2.circle(frame, (cx, cy), 15, (252, 165, 3), cv2.FILLED)
 
     # Length between fingers
-    length = math.hypot(x2-x1, y2-y1)
+    idxLength = math.hypot(i1-t1, i2-t2)
 
     # The mapped volume
-    vol = np.interp(length, [60, 300], [int(min_volume), int(max_volume)])
+    vol = np.interp(idxLength, [60, 300], [int(min_volume), int(max_volume)])
 
     print(vol)
-    osascript.osascript("set volume output volume {}".format(vol))
+
+    # Check if if fingers are up
+    fingers = detector.fingers_up(lm_list)
+    print("fingers: {}".format(fingers))
+
+
+    if setVol:
+      s = NSAppleScript.alloc().initWithSource_('set volume output volume {}'.format(vol))
+      s.executeAndReturnError_(None)
+
+      if fingers[2] != True:
+        setVol = False
+    else:
+      if fingers[2] != True:
+        setVol = True
 
     # Show green dot 
-    if length < 60:
+    if idxLength < 60:
       cv2.circle(frame, (cx, cy), 15, (144, 245, 66), cv2.FILLED)
-
   # Set fps
   curr_time = time.time()
   fps = 1 / (curr_time - prev_time)
   prev_time = curr_time
 
-  print("fps: {}".format(int(fps)))
+  print("vol: {} fps: {}".format(vol, int(fps)))
 
   cv2.putText(frame, f'FPS: {int(fps)}', (10, 70), cv2.FONT_HERSHEY_COMPLEX, 1, (252, 165, 3), 2)
   cv2.imshow("frame", frame)
   cv2.waitKey(1) & 0xFF
-
-wvs.stop()
--- modules/HandTrackingModule.py
+++ modules/HandTrackingModule.py
@@ -1,6 +1,5 @@
 import cv2
 import mediapipe as mp
-import time
 
 class HandDetector():
   def __init__(self, mode = False, max_hands = 2, detection_confidence = 0.5, tracking_confidence = 0.5):
@@ -13,11 +12,12 @@ class HandDetector():
     self.mp_hands = mp.solutions.hands
     self.hands = self.mp_hands.Hands(self.mode, self.max_hands, self.detection_confidence, self.tracking_confidence)
     self.mp_draw = mp.solutions.drawing_utils
+    self.tipIds = [4, 8, 12, 16, 20]
 
-  def find_hands(self, img, draw = True):
+  def find_hands(self, frame, draw = True):
     # Get the current image from video 
-    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    self.results = self.hands.process(img_rgb)
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    self.results = self.hands.process(frame_rgb)
 
     # Make sure we have landmarks
     if self.results.multi_hand_landmarks:
@@ -25,11 +25,11 @@ class HandDetector():
       for hand_mark in self.results.multi_hand_landmarks:
         if draw:
           # Drawing the landmarks on the hand
-          self.mp_draw.draw_landmarks(img, hand_mark, self.mp_hands.HAND_CONNECTIONS)
+          self.mp_draw.draw_landmarks(frame, hand_mark, self.mp_hands.HAND_CONNECTIONS)
 
-    return img
+    return frame
 
-  def find_position(self, img, hand_no = 0, draw = True):
+  def find_position(self, frame, hand_no = 0, draw = True):
     lm_list = []
 
     if self.results.multi_hand_landmarks:
@@ -37,46 +37,29 @@ class HandDetector():
 
       for id, lm in enumerate(hands.landmark):
         # Get the shape of the image(hand)
-        h, w, c = img.shape
+        h, w, c = frame.shape
         cx, cy = int(lm.x * w), int(lm.y * h)
         lm_list.append([id, cx, cy])
 
         if draw:
-          cv2.circle(img, (cx, cy), 12, (252, 165, 3), cv2.FILLED)
+          cv2.circle(frame, (cx, cy), 12, (252, 165, 3), cv2.FILLED)
 
     return lm_list
 
-def main():
-  # Get the fps
-  prev_time = 0
-  curr_time = 0
+  def fingers_up(self, lm_list):
+    fingers = []
 
-  # Start video 
-  cap =  cv2.VideoCapture(0)
-  detector = HandDetector()
+    # Thumb
+    if lm_list[self.tipIds[0]][1] < lm_list[self.tipIds[0]-1][1]:
+      fingers.append(1)
+    else:
+      fingers.append(0)
 
-  while True:
-    # Get the current image from video 
-    succ, img = cap.read()
-    
-    # Get image from detector
-    img = detector.find_hands(img)
-    # Get landmark list
-    lm_list = detector.find_position(img)
-    if len(lm_list) != 0:
-      print(lm_list[4])
+    for id in range(1, 5):
+      if lm_list[self.tipIds[id]][2] < lm_list[self.tipIds[id]-2][2]:
+        fingers.append(1)
+      else:
+        fingers.append(0)
+    return fingers
 
-    # Set fps
-    curr_time = time.time()
-    fps = 1 / (curr_time - prev_time)
-    prev_time = curr_time
 
-    # Show fps
-    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_COMPLEX, 3, (252, 165, 3), 3)
-
-    cv2.imshow("Image: ", img)
-    cv2.waitKey(1)
-    
-
-if __name__ == "__main__":
-  main()
