63fe6f44b3368dbe4eacf88bc9d379ede45d377f
Jay Jones <jaypjonesofficial@gmail.com>
2021-06-06 14:20:55 -0500

    Clean up

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -17,7 +17,7 @@ max_volume = 100
 area = 0
 
 # HandDetector Object
-detector = HandDetector(detection_confidence=0.7)
+detector = HandDetector(detection_confidence=0.7, max_hands=1)
 setVol = True
 
 while True:
@@ -27,6 +27,11 @@ while True:
   # Find hands
   frame = detector.find_hands(frame)
   lm_list, bb = detector.find_position(frame )
+
+  # Set fps
+  curr_time = time.time()
+  fps = 1 / (curr_time - prev_time)
+  prev_time = curr_time
 
   # Get current volume
   s = NSAppleScript.alloc().initWithSource_('output volume of (get volume settings)')
@@ -60,16 +65,11 @@ while True:
         cv2.circle(frame, (data[4], data[5]), 15, (144, 245, 66), cv2.FILLED)
 
 
-  # Display volume bar
-  cv2.rectangle(frame, (75, (400 - (2 * vol))), (10, 400), (201, 194, 117), cv2.FILLED)
-  cv2.putText(frame, 'Vol: {} %'.format(vol), (10, 440), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (201, 194, 117), 2)
-  # Set fps
-  curr_time = time.time()
-  fps = 1 / (curr_time - prev_time)
-  prev_time = curr_time
+    # Display volume bar
+    cv2.rectangle(frame, (75, (400 - (2 * vol))), (10, 400), (201, 194, 117), cv2.FILLED)
+    cv2.putText(frame, 'Vol: {} %'.format(vol), (10, 440), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (201, 194, 117), 2)
+    cv2.putText(frame, f'FPS: {int(fps)}', (10, 70), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (201, 194, 117), 2)
+    print("vol: {} fps: {}".format(vol, int(fps)))
 
-  print("vol: {} fps: {}".format(vol, int(fps)))
-
-  cv2.putText(frame, f'FPS: {int(fps)}', (10, 70), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (201, 194, 117), 2)
   cv2.imshow("frame", frame)
   cv2.waitKey(1) & 0xFF
