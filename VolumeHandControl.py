24bbde0d91a3e627ff89947851469bfbafb25c65
Jay Jones <jaypjonesofficial@gmail.com>
2021-06-06 14:14:15 -0500

    Setting volume when ring finger is down

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -12,7 +12,12 @@ wvs = WebcamVideoStream().start()
 # Get the fps
 prev_time = 0
 
-# HTM Object
+# Get volume settings
+min_volume = 0
+max_volume = 100
+area = 0
+
+# HandDetector Object
 detector = HandDetector(detection_confidence=0.7)
 setVol = True
 
@@ -22,60 +27,43 @@ while True:
 
   # Find hands
   frame = detector.find_hands(frame)
-  lm_list = detector.find_position(frame, draw=False)
+  lm_list, bb = detector.find_position(frame )
 
   # Get current volume
   s = NSAppleScript.alloc().initWithSource_('output volume of (get volume settings)')
   result, error_info = s.executeAndReturnError_(None)
   vol = int(result.stringValue())
 
-  # Display volume bar
-  cv2.rectangle(frame, (75, (302 - (2 * vol))), (10, 400), (252, 165, 3), cv2.FILLED)
-  cv2.putText(frame, 'Vol: {} %'.format(vol), (10, 440), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (252, 165, 3), 2)
-
   if len(lm_list) != 0:
-    # Get volume settings
-    min_volume = 0
-    max_volume = 100
+    # Filter based on area
+    area = (bb[2] - bb[0]) * (bb[3] - bb[1]) // 100
 
-    # Coords for the index finder and thumb
-    t1, t2 = lm_list[4][1], lm_list[4][2]
-    i1, i2 = lm_list[8][1], lm_list[8][2]
+    if 200 < area < 2000:
+      # Find distance between index and thumb
+      length, img, data = detector.get_distance(4, 8, frame)
 
-    # Center of line
-    cx, cy = (t1 + i1) // 2, (t2 + i2) // 2
+      # The mapped volume
+      volPer = np.interp(length, [50, 300], [0, 100])
+      smooth = 5
+      volPer = smooth * round(volPer/smooth)
+      vol = volPer
 
-    cv2.circle(frame, (t1, t2), 15, (252, 165, 3), cv2.FILLED)
-    cv2.circle(frame, (i1, i2), 15, (252, 165, 3), cv2.FILLED)
-    cv2.line(frame, (t1, t2), (i1, i2), (252, 165, 3), 3)
-    cv2.circle(frame, (cx, cy), 15, (252, 165, 3), cv2.FILLED)
+      # Check what fingers are up
+      fingers = detector.fingers_up()
 
-    # Length between fingers
-    idxLength = math.hypot(i1-t1, i2-t2)
-
-    # The mapped volume
-    vol = np.interp(idxLength, [60, 300], [int(min_volume), int(max_volume)])
-
-    print(vol)
-
-    # Check if if fingers are up
-    fingers = detector.fingers_up(lm_list)
-    print("fingers: {}".format(fingers))
+      # Set volume when ring finger is down
+      if not fingers[3]:
+        # Adjust the system volume
+        s = NSAppleScript.alloc().initWithSource_('set volume output volume {}'.format(vol))
+        s.executeAndReturnError_(None)
+        
+        # Show green dot 
+        cv2.circle(frame, (data[4], data[5]), 15, (144, 245, 66), cv2.FILLED)
 
 
-    if setVol:
-      s = NSAppleScript.alloc().initWithSource_('set volume output volume {}'.format(vol))
-      s.executeAndReturnError_(None)
-
-      if fingers[2] != True:
-        setVol = False
-    else:
-      if fingers[2] != True:
-        setVol = True
-
-    # Show green dot 
-    if idxLength < 60:
-      cv2.circle(frame, (cx, cy), 15, (144, 245, 66), cv2.FILLED)
+  # Display volume bar
+  cv2.rectangle(frame, (75, (400 - (2 * vol))), (10, 400), (201, 194, 117), cv2.FILLED)
+  cv2.putText(frame, 'Vol: {} %'.format(vol), (10, 440), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (201, 194, 117), 2)
   # Set fps
   curr_time = time.time()
   fps = 1 / (curr_time - prev_time)
@@ -83,6 +71,6 @@ while True:
 
   print("vol: {} fps: {}".format(vol, int(fps)))
 
-  cv2.putText(frame, f'FPS: {int(fps)}', (10, 70), cv2.FONT_HERSHEY_COMPLEX, 1, (252, 165, 3), 2)
+  cv2.putText(frame, f'FPS: {int(fps)}', (10, 70), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (201, 194, 117), 2)
   cv2.imshow("frame", frame)
   cv2.waitKey(1) & 0xFF
--- modules/HandTrackingModule.py
+++ modules/HandTrackingModule.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+import math
 
 class HandDetector():
   def __init__(self, mode = False, max_hands = 2, detection_confidence = 0.5, tracking_confidence = 0.5):
@@ -30,8 +31,13 @@ class HandDetector():
     return frame
 
   def find_position(self, frame, hand_no = 0, draw = True):
-    lm_list = []
+    x_list = []
+    y_list = []
+    bb = []
 
+    self.lm_list = []
+
+    # Make sure there are hands
     if self.results.multi_hand_landmarks:
       hands = self.results.multi_hand_landmarks[hand_no]
 
@@ -39,27 +45,62 @@ class HandDetector():
         # Get the shape of the image(hand)
         h, w, c = frame.shape
         cx, cy = int(lm.x * w), int(lm.y * h)
-        lm_list.append([id, cx, cy])
+        x_list.append(cx)
+        y_list.append(cy)
+        self.lm_list.append([id, cx, cy])
 
         if draw:
-          cv2.circle(frame, (cx, cy), 12, (252, 165, 3), cv2.FILLED)
+          cv2.circle(frame, (cx, cy), 8, (235, 228, 150), cv2.FILLED)
 
-    return lm_list
+      x_min, x_max = min(x_list), max(x_list)
+      y_min, y_max = min(y_list), max(y_list)
 
-  def fingers_up(self, lm_list):
+      bb = x_min, y_min, x_max, y_max
+
+      if draw:
+        cv2.rectangle(frame, (bb[0]-20, bb[1]-20), (bb[2]+20, bb[3]+20), (252, 165, 3), 2)
+
+    return self.lm_list, bb
+
+  def fingers_up(self):
+    multi_handedness = self.results.multi_handedness
+    handedness = multi_handedness[0].classification[0].label
     fingers = []
 
-    # Thumb
-    if lm_list[self.tipIds[0]][1] < lm_list[self.tipIds[0]-1][1]:
-      fingers.append(1)
+    # Thumb for both hands
+    if handedness == "Right":
+      if self.lm_list[self.tipIds[0]][1] < self.lm_list[self.tipIds[0]-1][1]:
+        fingers.append(1)
+      else:
+        fingers.append(0)
     else:
-      fingers.append(0)
+      if self.lm_list[self.tipIds[0]][1] > self.lm_list[self.tipIds[0]-1][1]:
+        fingers.append(1)
+      else:
+        fingers.append(0)
 
     for id in range(1, 5):
-      if lm_list[self.tipIds[id]][2] < lm_list[self.tipIds[id]-2][2]:
+      if self.lm_list[self.tipIds[id]][2] < self.lm_list[self.tipIds[id]-2][2]:
         fingers.append(1)
       else:
         fingers.append(0)
     return fingers
 
+  def get_distance(self, p1, p2, frame, draw = True):
+    # Coords for the two fingers
+    x1, x2 = self.lm_list[p1][1], self.lm_list[p1][2]
+    y1, y2 = self.lm_list[p2][1], self.lm_list[p2][2]
 
+    # Center of line
+    cx, cy = (x1 + y1) // 2, (x2 + y2) // 2
+
+    if draw:
+      # Display lines between two fingers
+      cv2.circle(frame, (x1, x2), 15, (252, 165, 3), cv2.FILLED)
+      cv2.circle(frame, (y1, y2), 15, (252, 165, 3), cv2.FILLED)
+      cv2.line(frame, (x1, x2), (y1, y2), (252, 165, 3), 3)
+      cv2.circle(frame, (cx, cy), 15, (252, 165, 3), cv2.FILLED)
+
+    length = math.hypot(y1-x1, y2-x2)
+
+    return length, frame, [x1 ,y1, x2, y2, cx, cy]
